5e9ee7269ff3cdc4610c643ee58c300c52964dab
deepan1413 <deepan14132002@gmail.com>
2025-12-26 15:28:07 +0530

    first commit

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme=ThemeData(colorScheme: ColorScheme.dark(
+  primary: Colors.black
+));
---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+ThemeData lightMode=ThemeData(colorScheme: ColorScheme.light(
+  primary: Colors.white
+));
